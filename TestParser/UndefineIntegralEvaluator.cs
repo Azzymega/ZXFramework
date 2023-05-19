b35de07639b0d1bd0ec02036a35f248e5b745a41
Konrada <jjj@mail.ru>
2023-05-19 13:25:00 +0300

    QWFramework Ревизия 1.7.

--- TestParser/UndefineIntegralEvaluator.cs
+++ TestParser/UndefineIntegralEvaluator.cs
@@ -1,11 +1,21 @@
 ﻿namespace QWFramework;
-// QWFramework Rev 1.4
+// QWFramework Rev 1.7
 public class UndefineIntegralEvaluator : AEvaluator
 {
     public UndefineIntegralEvaluator(string equation) : base(equation) { }
 
     public override int FindArgument(string equation)
     {
+        int count = 0;
+        foreach (char c in equation)
+        {
+            if (Char.IsDigit(c))
+            {
+                count++;
+            }
+        }
+        if (count > 1)
+        {
         bool minus = false;
         string Argument = new string("0");
         for (int i = 0; i < equation.Length; i++)
@@ -24,6 +34,28 @@ public class UndefineIntegralEvaluator : AEvaluator
             return -1*int.Parse(Argument);
         }
         return int.Parse(Argument);
+        }
+        else
+        {
+            bool minus = false;
+            string Argument = new string("0");
+            for (int i = 0; i < equation.Length; i++)
+            {
+                if (Char.IsDigit(equation[i]))
+                {
+                    Argument += equation[i];
+                }
+                if (equation[i] == '-')
+                {
+                    minus = true;
+                }
+            }
+            if (minus == true)
+            {
+                return -1 * int.Parse(Argument);
+            }
+            return int.Parse(Argument);
+        }
     }
 
     protected override string ReturnEquationWithoutArgument(string equation)
